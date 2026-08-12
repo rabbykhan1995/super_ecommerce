a90bbfc32de27bcc1db3d680a02a5ef84ff70d63
MD Rabbi Khan <rabbykhan082020@gmail.com>
2026-08-12 23:02:13 +0600

    update ecommere action file

--- ecommerce/app/actions.ts
+++ ecommerce/app/actions.ts
@@ -1,20 +1,20 @@
-"use server";
-import { revalidateTag } from "next/cache";
+// "use server";
+// import { revalidateTag } from "next/cache";
 
-export async function refreshBlogData(slug: string) {
-  if (slug) {
-    revalidateTag(`blog-${slug}`);
-  }
-}
+// export async function refreshBlogData(slug: string) {
+//   if (slug) {
+//     revalidateTag(`blog-${slug}`);
+//   }
+// }
 
-export async function refreshTrainingData(slug: string) {
-  if (slug) {
-    revalidateTag(`training-${slug}`);
-  }
-}
+// export async function refreshTrainingData(slug: string) {
+//   if (slug) {
+//     revalidateTag(`training-${slug}`);
+//   }
+// }
 
-export async function refreshProductData(slug: string) {
-  if (slug) {
-    revalidateTag(`product-${slug}`);
-  }
-}
+// export async function refreshProductData(slug: string) {
+//   if (slug) {
+//     revalidateTag(`product-${slug}`);
+//   }
+// }
